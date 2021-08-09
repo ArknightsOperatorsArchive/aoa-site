d65158367d01afee41a9be8e071bef305fa09726
Eric Jiang <ericjiang97@gmail.com>
2021-08-09 20:05:35 +0800

    improve nav

--- containers/AdminContainers/AdminDashboardContainer.tsx
+++ containers/AdminContainers/AdminDashboardContainer.tsx
@@ -1,33 +1,21 @@
 import React, { Fragment, useState } from "react";
-import { Dialog, Menu, Transition } from "@headlessui/react";
-import {
-  ClockIcon,
-  HomeIcon,
-  MenuAlt1Icon,
-  ViewListIcon,
-  XIcon,
-} from "@heroicons/react/outline";
-import {
-  ChevronRightIcon,
-  DotsVerticalIcon,
-  DuplicateIcon,
-  PencilAltIcon,
-  SearchIcon,
-  SelectorIcon,
-  TrashIcon,
-  UserAddIcon,
-} from "@heroicons/react/solid";
+import { Dialog, Transition } from "@headlessui/react";
+import { HomeIcon, ViewListIcon, XIcon } from "@heroicons/react/outline";
 import AdminAuthenticatedContainer from "./AdminAuthenticatedContainer";
 import AdminAuthorisedContainer from "./AdminAuthorisedContainer";
 import AccountDropdown from "../account/AccountDropdown";
+import { useRouter } from "next/router";
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(" ");
 }
 const navigation = [
-  { name: "Home", href: "#", icon: HomeIcon, current: true },
-  { name: "My tasks", href: "#", icon: ViewListIcon, current: false },
-  { name: "Recent", href: "#", icon: ClockIcon, current: false },
+  { name: "Home", href: "/admin", icon: HomeIcon },
+  {
+    name: "Manage Operators",
+    href: "/admin/operators",
+    icon: ViewListIcon,
+  },
 ];
 
 export interface AdminDashboardContainerProps {
@@ -39,8 +27,10 @@ const AdminDashboardContainer: React.FC<AdminDashboardContainerProps> = ({
   controls,
   pageTitle,
 }) => {
+  const router = useRouter();
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
+  const currentPath = router.pathname;
   return (
     <AdminAuthenticatedContainer>
       <AdminAuthorisedContainer>
@@ -102,30 +92,36 @@ const AdminDashboardContainer: React.FC<AdminDashboardContainerProps> = ({
                   <div className="mt-5 flex-1 h-0 overflow-y-auto">
                     <nav className="px-2">
                       <div className="space-y-1">
-                        {navigation.map((item) => (
-                          <a
-                            key={item.name}
-                            href={item.href}
-                            className={classNames(
-                              item.current
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-600 hover:text-gray-900 hover:bg-gray-50",
-                              "group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md"
-                            )}
-                            aria-current={item.current ? "page" : undefined}
-                          >
-                            <item.icon
+                        {navigation.map((item) => {
+                          const current = currentPath === item.href;
+                          console.log(currentPath, current);
+                          return (
+                            <button
+                              key={item.name}
+                              onClick={() => {
+                                router.push(item.href);
+                              }}
                               className={classNames(
-                                item.current
-                                  ? "text-gray-500"
-                                  : "text-gray-400 group-hover:text-gray-500",
-                                "mr-3 flex-shrink-0 h-6 w-6"
+                                current
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-gray-600 hover:text-gray-900 hover:bg-gray-50",
+                                "group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md"
                               )}
-                              aria-hidden="true"
-                            />
-                            {item.name}
-                          </a>
-                        ))}
+                              aria-current={current ? "page" : undefined}
+                            >
+                              <item.icon
+                                className={classNames(
+                                  current
+                                    ? "text-gray-500"
+                                    : "text-gray-400 group-hover:text-gray-500",
+                                  "mr-3 flex-shrink-0 h-6 w-6"
+                                )}
+                                aria-hidden="true"
+                              />
+                              {item.name}
+                            </button>
+                          );
+                        })}
                       </div>
                     </nav>
                   </div>
@@ -147,57 +143,39 @@ const AdminDashboardContainer: React.FC<AdminDashboardContainerProps> = ({
               <div className="h-0 flex-1 flex flex-col overflow-y-auto">
                 {/* User account dropdown */}
                 <AccountDropdown />
-                {/* Sidebar Search */}
-                <div className="px-3 mt-5">
-                  <label htmlFor="search" className="sr-only">
-                    Search
-                  </label>
-                  <div className="mt-1 relative rounded-md shadow-sm">
-                    <div
-                      className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
-                      aria-hidden="true"
-                    >
-                      <SearchIcon
-                        className="mr-3 h-4 w-4 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <input
-                      type="text"
-                      name="search"
-                      id="search"
-                      className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-9 sm:text-sm border-gray-300 rounded-md"
-                      placeholder="Search"
-                    />
-                  </div>
-                </div>
                 {/* Navigation */}
                 <nav className="px-3 mt-6">
                   <div className="space-y-1">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-200 text-gray-900"
-                            : "text-gray-700 hover:text-gray-900 hover:bg-gray-50",
-                          "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        <item.icon
+                    {navigation.map((item) => {
+                      const current = currentPath === item.href;
+                      console.log(currentPath, current);
+                      return (
+                        <button
+                          key={item.name}
+                          onClick={() => {
+                            router.push(item.href);
+                          }}
                           className={classNames(
-                            item.current
-                              ? "text-gray-500"
-                              : "text-gray-400 group-hover:text-gray-500",
-                            "mr-3 flex-shrink-0 h-6 w-6"
+                            current
+                              ? "bg-gray-200 text-gray-900"
+                              : "text-gray-700 hover:text-gray-900 hover:bg-gray-50",
+                            "group flex items-center px-2 py-2 text-sm font-medium rounded-md w-full"
                           )}
-                          aria-hidden="true"
-                        />
-                        {item.name}
-                      </a>
-                    ))}
+                          aria-current={current ? "page" : undefined}
+                        >
+                          <item.icon
+                            className={classNames(
+                              current
+                                ? "text-gray-500"
+                                : "text-gray-400 group-hover:text-gray-500",
+                              "mr-3 flex-shrink-0 h-6 w-6"
+                            )}
+                            aria-hidden="true"
+                          />
+                          {item.name}
+                        </button>
+                      );
+                    })}
                   </div>
                 </nav>
               </div>

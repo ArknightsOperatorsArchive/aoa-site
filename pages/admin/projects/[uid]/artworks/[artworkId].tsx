e0a2f4f338720c3fb93988261c9ee3f3cef4515f
Eric Jiang <ericjiang97@gmail.com>
2021-08-14 13:55:26 +0800

    improvements to artwork management page

---
+++ pages/admin/projects/[uid]/artworks/[artworkId].tsx
@@ -0,0 +1,102 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+import AdminDashboardContainer from "../../../../../containers/AdminContainers/AdminDashboardContainer";
+import { useFunctions } from "../../../../../firebase/firebase";
+import { Nullable } from "../../../../../types";
+import Artwork from "../../../../../types/Artwork";
+import Loading from "../../../../../components/Loading";
+import ErrorContainer from "../../../../../components/Error";
+
+const ArtworkManagementPage = () => {
+  const functions = useFunctions();
+  const router = useRouter();
+  const { uid, artworkId } = router.query;
+
+  const [loaded, setIsLoaded] = useState(false);
+  const [errored, setErrored] = useState(false);
+  const [error, setError] = useState<Nullable<Error>>(null);
+
+  const [artwork, setArtwork] = useState<Nullable<Artwork>>(null);
+
+  useEffect(() => {
+    function getData() {
+      setErrored(false);
+      const getArtwork = functions.httpsCallable("getArtwork");
+      getArtwork({ projectId: uid, artworkId: artworkId })
+        .then((result) => {
+          console.log(result);
+          const data = result.data as Artwork;
+          return data;
+        })
+        .then((data) => {
+          setArtwork(data);
+          setIsLoaded(true);
+          setErrored(false);
+        })
+        .catch((err) => {
+          setArtwork(null);
+          setIsLoaded(true);
+          setErrored(true);
+          setError(err);
+          console.error(err);
+        });
+    }
+    getData();
+  }, [functions, uid, artworkId]);
+
+  if (!loaded) {
+    return (
+      <AdminDashboardContainer
+        pageTitle={"Manage Artwork - Loading..."}
+        controls={
+          <Fragment>
+            <button
+              type="button"
+              onClick={() => router.push("/admin/projects/artworks/create")}
+              className="order-0 inline-flex items-center px-4 py-2 border shadow-sm text-sm font-medium rounded-md text-blue-900 border-blue-600 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3"
+            >
+              Add Artwork
+            </button>
+          </Fragment>
+        }
+      >
+        <Loading loadingMessage="Grabbing artwork details..." />
+      </AdminDashboardContainer>
+    );
+  }
+
+  if (errored) {
+    return (
+      <AdminDashboardContainer
+        pageTitle={"Manage Artwork - Errored"}
+        controls={
+          <Fragment>
+            <button
+              type="button"
+              onClick={() => router.push("/admin/projects/artworks/create")}
+              className="order-0 inline-flex items-center px-4 py-2 border shadow-sm text-sm font-medium rounded-md text-blue-900 border-blue-600 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3"
+            >
+              Add Artwork
+            </button>
+          </Fragment>
+        }
+      >
+        <ErrorContainer>
+          <h2 className="text-xl font-semibold">Error Occured</h2>
+          <h3 className="text-md font-regular text-grey-300">Error stack:</h3>
+          {error && <div>{error.stack}</div>}
+        </ErrorContainer>
+      </AdminDashboardContainer>
+    );
+  }
+
+  return (
+    <AdminDashboardContainer pageTitle="Manage Artwork">
+      Manage Artworks here.
+      <pre>{JSON.stringify(artwork, null, 2)}</pre>
+    </AdminDashboardContainer>
+  );
+};
+
+export default ArtworkManagementPage;
--- pages/admin/projects/[uid]/index.tsx
+++ pages/admin/projects/[uid]/index.tsx
@@ -1,22 +1,20 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import AdminDashboardContainer from "../../../containers/AdminContainers/AdminDashboardContainer";
-import { useFunctions } from "../../../firebase/firebase";
+import AdminDashboardContainer from "../../../../containers/AdminContainers/AdminDashboardContainer";
+import { useFunctions } from "../../../../firebase/firebase";
 
-import Artwork from "../../../types/Artwork";
-import { Nullable } from "../../../types";
-import StatusLabels from "../../../components/StatusLabels";
-import Loading from "../../../components/Loading";
-import ErrorContainer from "../../../components/Error";
+import Artwork from "../../../../types/Artwork";
+import { Nullable } from "../../../../types";
+import StatusLabels from "../../../../components/StatusLabels";
+import Loading from "../../../../components/Loading";
+import ErrorContainer from "../../../../components/Error";
 
 const ProjectPage = () => {
-  const [addArtworkModalOpen, setAddArtworkModalOpen] = useState(false);
   const functions = useFunctions();
   const router = useRouter();
   const { uid } = router.query;
 
   const [loaded, setIsLoaded] = useState(false);
-  const [modalOpen, setModalOpen] = useState(false);
   const [errored, setErrored] = useState(false);
   const [error, setError] = useState<Nullable<Error>>(null);
   const [projectArtworks, setProjectArtworks] =
@@ -72,7 +70,7 @@ const ProjectPage = () => {
       </AdminDashboardContainer>
     );
   }
-  console.info(`Errored: ${errored}`);
+
   if (errored) {
     return (
       <AdminDashboardContainer
@@ -97,58 +95,61 @@ const ProjectPage = () => {
       </AdminDashboardContainer>
     );
   }
+
   return (
-    <React.Fragment>
-      <AdminDashboardContainer
-        pageTitle={"Manage Project"}
-        controls={
-          <Fragment>
-            <button
-              type="button"
-              onClick={() => router.push("/admin/projects/artworks/create")}
-              className="order-0 inline-flex items-center px-4 py-2 border shadow-sm text-sm font-medium rounded-md text-blue-900 border-blue-600 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3"
-            >
-              Add Artwork
-            </button>
-          </Fragment>
-        }
-      >
-        <div className="flex-1 min-w-0 px-3 py-2">
-          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-            Artworks
-          </h2>
-          {projectArtworks && (
-            <ul className="mt-3 -my-5 divide-y divide-gray-200">
-              {projectArtworks.map((artwork) => (
-                <li key={artwork.uid} className="py-4">
-                  <div className="flex items-center space-x-4">
-                    <div className="flex-1 min-w-0">
-                      <div className="flex items-center">
-                        <p className="text-sm font-medium text-gray-900 truncate mr-2">
-                          {artwork.operator.name}
-                        </p>
-                        <StatusLabels status={artwork.status} />
-                      </div>
-                      <p className="text-sm text-gray-500 truncate">
-                        {artwork.artist.displayName}
+    <AdminDashboardContainer
+      pageTitle={"Manage Project"}
+      controls={
+        <Fragment>
+          <button
+            type="button"
+            onClick={() => router.push("/admin/projects/artworks/create")}
+            className="order-0 inline-flex items-center px-4 py-2 border shadow-sm text-sm font-medium rounded-md text-blue-900 border-blue-600 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:order-1 sm:ml-3"
+          >
+            Add Artwork
+          </button>
+        </Fragment>
+      }
+    >
+      <div className="flex-1 min-w-0 px-3 py-2">
+        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+          Artworks
+        </h2>
+        {projectArtworks && (
+          <ul className="mt-3 -my-5 divide-y divide-gray-200">
+            {projectArtworks.map((artwork) => (
+              <li key={artwork.uid} className="py-4">
+                <div className="flex items-center space-x-4">
+                  <div className="flex-1 min-w-0">
+                    <div className="flex items-center">
+                      <p className="text-sm font-medium text-gray-900 truncate mr-2">
+                        {artwork.operator.name}
                       </p>
+                      <StatusLabels status={artwork.status} />
                     </div>
-                    <div>
-                      <a
-                        href="#"
-                        className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
-                      >
-                        View
-                      </a>
-                    </div>
+                    <p className="text-sm text-gray-500 truncate">
+                      {artwork.artist.displayName}
+                    </p>
                   </div>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </AdminDashboardContainer>
-    </React.Fragment>
+                  <div>
+                    <button
+                      onClick={() => {
+                        router.push(
+                          `/admin/projects/${uid}/artworks/${artwork.uid}`
+                        );
+                      }}
+                      className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
+                    >
+                      View
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </AdminDashboardContainer>
   );
 };
 

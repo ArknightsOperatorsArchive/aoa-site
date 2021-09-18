95ee53b45e336dd1375e00ff147b55555134ef71
Eric Jiang <ericjiang97@gmail.com>
2021-09-18 11:47:42 +0800

    add searchApi

--- config/algolia.ts
+++ config/algolia.ts
@@ -1,6 +1,13 @@
+import algoliasearch from "algoliasearch";
+
 export const ALGOLIA_CONFIG = {
   APP_ID: process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ID,
   API_KEY: process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY,
 };
 
-export default ALGOLIA_CONFIG;
+const searchClient = algoliasearch(
+  ALGOLIA_CONFIG.APP_ID as string,
+  ALGOLIA_CONFIG.API_KEY as string
+);
+
+export default searchClient;

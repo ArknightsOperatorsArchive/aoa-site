5dbf720757bfa3bf183c36a37b628608e5d3c12f
Eric <eric@ericjiang.dev>
2022-01-02 11:24:21 +0800

    replacing search with frontend searching

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,29 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NextPageContext } from "next";
 import Link from "next/link";
 
 import CoreContainer from "../containers/main/CoreContainer";
 
 import ArtistQueryResponse from "../types/SearchResponse";
-import { paginate } from "../utils/paginate";
-import {
-  ChevronDoubleLeftIcon,
-  ChevronDoubleRightIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-} from "@heroicons/react/outline";
 import { useRouter } from "next/router";
 
 interface SearchPageProps {
   data: ArtistQueryResponse;
   searchQuery: string;
 }
-const SearchPage: React.FC<SearchPageProps> = ({ data, searchQuery }) => {
+const SearchPage: React.FC<SearchPageProps> = ({ searchQuery }) => {
   const [searchTerm, setSearchTerm] = useState(searchQuery);
+
+  useEffect(() => {
+    function fetchData() {
+      console.log('fetching data!')
+    }
+    fetchData()
+  })
+
   const router = useRouter();
   return (
     <CoreContainer>
-      Test
+      <div className="py-4 px-2">
+        <div className="font-black uppercase italic leading-6 flex-1">
+          <h2 className="text-7xl">search.</h2>
+          <div className="mt-3">
+            <input
+              type="text"
+              name="search"
+              id="search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.currentTarget.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  router.push(`/search?q=${searchTerm}`);
+                }
+              }}
+              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+              placeholder="search for an artist or operator..."
+            />
+          </div>
+        </div>
+      </div>
     </CoreContainer>
   );
 };
@@ -31,16 +52,13 @@ const SearchPage: React.FC<SearchPageProps> = ({ data, searchQuery }) => {
 export const getServerSideProps = async (ctx: NextPageContext) => {
   const { query } = ctx;
   const searchTarget = (query["q"] as string) || "";
-  const pageNumber = query["page"] as string;
-  const pageSize = query["size"] as string;
 
-  // const results = await search(searchTarget, pageNumber, pageSize);
   return {
     props: {
-      data: { hits: [] },
       searchQuery: searchTarget,
     },
   };
 };
+
 
 export default SearchPage;
